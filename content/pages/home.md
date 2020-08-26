26339333a0df9cf17f8ef9d928796bbc6d47482e
mrkaluzny <wojciech.kaluzny@me.com>
2020-08-26 21:45:30 +0200

    Added content from netlify to index page

--- content/pages/home.md
+++ content/pages/home.md
@@ -5,7 +5,7 @@ title: Henlo
 links:
   - link:
       content: Documentation
-      url: https://github.com/clean-commit/gatsby-starter-henlo
+      url: https://github.com/clean-commit/gatsby-starter-henlo#readme
   - link:
       content: Clean Commit
       url: https://cleancommit.io
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,14 @@ import ReactSVG from 'react-svg'
 import Layout from '@/components/Layout'
 import GitHubButton from 'react-github-btn'
 
-export const HomePageTemplate = ({ }) => {
+export const HomePageTemplate = ({ data }) => {
   return (
     <>
       <div className='min-h-screen flex items-center justify-center'>
         <div className="container px-5 sm:px-10">
           <section className="w-full bg-teal-100 py-20 px-5 rounded-lg shadow border-grey-lighter border">
             <div className='text-center mx-auto'>
-              <ReactSVG src="/img/logo.svg" class="logo"></ReactSVG>
+              <ReactSVG src="/img/logo.svg" className="logo"></ReactSVG>
             </div>
             <div className="text-center mt-6">
               <span className="mr-3">
@@ -21,10 +21,15 @@ export const HomePageTemplate = ({ }) => {
                 <GitHubButton href="https://github.com/clean-commit/gatsby-starter/archive/master.zip" data-color-scheme="no-preference: dark; light: light; dark: dark;" data-icon="octicon-download" aria-label="Download clean-commit/gatsby-starter on GitHub">Download</GitHubButton>
               </span>
             </div>
-            <div className="links">
-              <a href="https://github.com/clean-commit/gatsby-starter" target="_blank" className="links__item">Documentation</a>
-              <a href="https://cleancommit.io" target="_blank" className="links__item">Clean Commit</a>
-            </div>
+
+
+            {data.links.length > 0 ? <div className="links">
+              {
+                data.links.map(({ link: link }, i) => {
+                  return <a href={link.url} key={i} target="_blank" className="links__item">{link.content}</a>
+                })
+              }
+            </div> : ''}
           </section>
         </div>
       </div>
@@ -32,24 +37,41 @@ export const HomePageTemplate = ({ }) => {
   )
 }
 
-class HomePage extends React.Component {
-  render() {
-    const { data } = this.props
+const HomePage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
 
-    return (
-      <Layout hideNav={true}>
-        <HomePageTemplate />
-      </Layout>
-    )
-  }
+  return (
+
+    <Layout hideNav={true}>
+      <HomePageTemplate data={frontmatter} />
+    </Layout>
+  )
 }
 
 HomePage.propTypes = {
   data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
     }),
   }),
 }
+
+export const pageQuery = graphql`
+  query HomePageTemplate {
+    markdownRemark(frontmatter: { template: { eq: "index" } }) {
+      frontmatter {
+        title
+        links {
+          link {
+            content
+            url
+          }
+        }
+      }
+    }
+    
+  }
+`
+
 
 export default HomePage
