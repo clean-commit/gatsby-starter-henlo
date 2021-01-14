8afa8560f1a3d519fac0916031e428a7f7c615f6
mrkaluzny <wojciech.kaluzny@me.com>
2021-01-14 16:24:35 +0100

    Seo component updated

--- src/components/SEO/Seo.js
+++ src/components/SEO/Seo.js
@@ -18,40 +18,27 @@ function SEO({ data, children }) {
     }
   `)
 
+  console.log(data)
+
   const metadata = meta.site.siteMetadata
   const metaDescription = data.description || metadata.description
   const title = data.title || metadata.title
+  const image = data.image
+    ? data.image.childImageSharp.fluid.src
+    : metadata.image
+
+  const fullTitle = `${title} ${metadata.separator} ${metadata.baseTitle}`
 
   return (
     <Helmet
       title={title}
-      titleTemplate={`%s ${metadata.separator} ${metadata.baseTitle}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]}>
+      titleTemplate={`%s ${metadata.separator} ${metadata.baseTitle}`}>
+      <meta name='description' content={metaDescription} />
+      <meta property='og:title' content={fullTitle} />
+      <meta property='og:image' content={image} />
+      <meta property='og:description' content={metaDescription} />
+      <meta name='twitter:title' content={fullTitle} />
+      <meta name='twitter:description' content={metaDescription} />
       {children}
     </Helmet>
   )
