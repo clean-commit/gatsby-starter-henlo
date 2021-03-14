096ac1aa903e9dc8c53ed952f7ef5cd18c2dc1bd
mrkaluzny <wojciech.kaluzny@me.com>
2021-03-14 15:26:10 +0100

    SVG are now used as components

---
+++ src/icons/social/Facebook.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-facebook"
+      viewBox="0 0 24 24"
+    >
+      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+    </svg>
+  );
+}
+
+export default Icon;
