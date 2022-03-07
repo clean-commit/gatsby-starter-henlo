a1f19f935cc3df1572c63fe2c59a5b2fbab355dd
mrkaluzny <wojciech.kaluzny@me.com>
2022-03-07 21:26:46 +0100

    Cleaned up styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import PropTypes from 'prop-types'
-import ReactMarkdown from 'react-markdown/with-html'
+import React from 'react';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown/with-html';
 
-import SEO from '@/components/SEO/Seo'
-import Layout from '@/components/Layout'
+import Seo from '@/components/SEO/Seo';
+import Layout from '@/components/Layout';
 
-import Logo from '@/icons/Logo'
-
+import Logo from '@/icons/Logo';
 
 const HomePageTemplate = ({ data }) => {
   return (
     <>
       <div className='min-h-screen flex items-center justify-center'>
         <div className='container px-5 sm:px-10'>
-          <section className='w-full bg-teal-100 py-20 px-5 rounded-lg shadow border-grey-lighter border'>
+          <section className='w-full py-20 px-5 rounded-lg shadow border-grey-lighter border'>
             <div className='text-center mx-auto'>
-              <div className='logo'>
-                <Logo />
+              <div>
+                <Logo className='mx-auto w-auto h-8 w-auto' />
               </div>
-               <p className="text-xs mt-2">{data.version}</p>
+              <p className='text-xs mt-2'>{data.version}</p>
             </div>
-            <ReactMarkdown children={data.description} className="text-center mt-6" allowDangerousHtml />
+            <ReactMarkdown
+              children={data.description}
+              className='text-center mt-6'
+              allowDangerousHtml
+            />
             {data.links.length > 0 ? (
               <div className='flex justify-center flex-wrap items-center mt-6'>
                 {data.links.map(({ link }, i) => {
@@ -34,7 +37,7 @@ const HomePageTemplate = ({ data }) => {
                       className='text-sm m-2'>
                       {link.content}
                     </a>
-                  )
+                  );
                 })}
               </div>
             ) : (
@@ -44,19 +47,19 @@ const HomePageTemplate = ({ data }) => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 const HomePage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
-      <SEO data={frontmatter.seo} />
+      <Seo data={frontmatter.seo} />
       <HomePageTemplate data={frontmatter} />
     </Layout>
-  )
-}
+  );
+};
 
 HomePage.propTypes = {
   data: PropTypes.shape({
@@ -64,7 +67,7 @@ HomePage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
 export const pageQuery = graphql`
   query HomePageTemplate {
@@ -83,6 +86,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
 
-export default HomePage
+export default HomePage;
