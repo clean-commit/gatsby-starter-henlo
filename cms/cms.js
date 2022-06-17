875ae7253b55a75da5cc64a08f6285d0b0aafca3
mrkaluzny <wojciech.kaluzny@me.com>
2022-06-17 18:03:35 +0200

    CMS updated

--- cms/cms.js
+++ cms/cms.js
@@ -6,7 +6,6 @@ import pages from './collections/pages';
 import posts from './collections/posts';
 import authors from './collections/authors';
 import settings from './collections/settings';
-import data from '../src/settings/seo.json';
 import PagePreview from './previews/Page';
 
 window.CMS_MANUAL_INIT = true;
@@ -14,7 +13,7 @@ window.CMS_MANUAL_INIT = true;
 const config = {
   config: {
     load_config_file: false,
-    display_url: data.siteUrl,
+    display_url: process.env.GATSBY_APP_URL,
     local_backend: true,
     backend: {
       name: 'git-gateway',
