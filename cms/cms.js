26933ac72bbbac1a6825df3541124d8036ec669b
Wojciech Kałużny <wk@cleancommit.io>
2024-03-26 15:58:26 +0100

    Update cms.js - fixed editor

--- cms/cms.js
+++ cms/cms.js
@@ -29,6 +29,18 @@ const config = {
   },
 };
 
+const injectCustomStyle = () => {
+  const style = document.createElement('style')
+  style.innerHTML = `
+    div[data-slate-editor] {
+      -webkit-user-modify: read-write !important;
+    }
+  `
+  document.head.appendChild(style)
+}
+
+injectCustomStyle()
+
 CMS.registerPreviewStyle('../commons.css');
 CMS.registerPreviewTemplate('pages', PagePreview);
 
